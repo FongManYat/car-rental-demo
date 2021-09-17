e4c32b062da0152a59ecd1613e59dcdc164335e9
FongManYat <fwyenergy@foxmail.com>
2021-09-18 02:10:00 +0800

    InitialCommit

---
+++ src/test/java/com/fwy/carrentaldemo/CarRentalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwy.carrentaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
